997ddef0e3db30e65cd41097a0dc5fff92412ac5
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-15 14:21:03 -0600

    Add  user auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "dashboard#index"
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  resources :users, only: [:show, :index]
+
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/dashboard', to: 'dashboard#index'
+
 end
