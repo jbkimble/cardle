927def944daf839e92db7d8e029ef7c710c11ea4
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-15 16:29:05 -0600

    Add flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "#{@user.first_name} #{@user.last_initial.capitalize}! Welcome to Cardle. Checkout our quick start guide or just click around!"
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      flash[:failure] = "Account creation failed, please try again"
+      redirect_to signup_path
+    end
   end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:id, :first_name, :last_initial, :email, :password, :avatar)
+    end
 end
