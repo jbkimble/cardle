1eaf16ea3c3116b1c0aaee80e7d672c2e7cf9937
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-16 18:35:38 -0600

    Edit flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "#{@user.first_name} #{@user.last_initial.capitalize}! Welcome to Cardle. Checkout our quick start guide or just click around!"
+      flash[:success] = "#{@user.first_name} #{@user.last_initial.capitalize}! Welcome to Cardle.
+                        Checkout our quick start guide or just click around so we can get to know each other!"
       session[:user_id] = @user.id
       redirect_to dashboard_path
     else
-      flash[:failure] = "Account creation failed, please try again"
       redirect_to signup_path
     end
   end
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:id, :first_name, :last_initial, :email, :password, :avatar)
+      params.require(:user).permit(:id, :first_name, :last_initial, :email, :password, :password_confirmation, :avatar)
     end
 end
