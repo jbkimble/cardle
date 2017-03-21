38212e7c016e6c577a01c98babc81507aa4e5719
Jon Kimble <jonathanbkimble@gmail.com>
2017-03-21 11:05:27 -0600

    User login/logout methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      flash[:success] = "Successfully logged in!"
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      flash[:failure] = "Something went wrong!"
+      render :new
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    flash[:success] = "Successfully logged out!"
+    redirect_to root_path
   end
 
 end
